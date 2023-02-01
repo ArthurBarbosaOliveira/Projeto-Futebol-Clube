6e1cb079c4be6d0b522e657bdb44dd4fe2f04eeb
ArthurBarbosaOliveira <arthurbarbosa93@gmail.com>
2023-02-01 19:41:08 -0300

    adicionando no MSC as condições para verificar o inProgress para atender os req 20 e 21

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -2,7 +2,13 @@ import { Request, Response } from 'express';
 import service from '../services/matchesServicer';
 
 const list = async (_req: Request, res: Response) => {
-  const check = await service.list();
+  let check = await service.list();
+  if (_req.query.inProgress) {
+    const { inProgress } = _req.query;
+    check = await service.progress(inProgress === 'true');
+  } else {
+    check = await service.list();
+  }
   return res.status(200)
     .json(check.message);
 };
--- app/backend/src/services/matchesServicer.ts
+++ app/backend/src/services/matchesServicer.ts
@@ -3,14 +3,27 @@ import model from '../database/models/Matches';
 import modelTeams from '../database/models/Teams';
 
 const list = async (): Promise<IResponse> => {
-  const result = await model.findAll({ include: [
+  const check = await model.findAll({ include: [
     { model: modelTeams, as: 'homeTeam', attributes: { exclude: ['id'] } },
     { model: modelTeams, as: 'awayTeam', attributes: { exclude: ['id'] } },
   ],
   });
-  return { type: null, message: result };
+  return { type: null, message: check };
+};
+
+const progress = async (inProgress: boolean):Promise<IResponse> => {
+  const check = await model.findAll({
+    where: { inProgress },
+    include: [
+      { model: modelTeams, as: 'homeTeam', attributes: { exclude: ['id'] } },
+      { model: modelTeams, as: 'awayTeam', attributes: { exclude: ['id'] } },
+    ],
+  });
+  if (!check) return { type: 404, message: 'Not found' };
+  return { type: null, message: check };
 };
 
 export default {
   list,
+  progress,
 };
